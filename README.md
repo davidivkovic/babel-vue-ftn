698e2e1fc8100f4d0dff4eea868a0de280c0d382
David Ivkovic <gorajkora@gmail.com>
2021-12-27 15:03:21 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,290 @@
+# babel-transform-vue-ftn
+Transpiles a vue-cli project into a non-bundled, non node-dependent project.
+
+## Goals
+The purpose of this tool is compiling away Single File Components (SFCs) whilst making the resulting 
+code look like it was written by hand.
+
+This tool was build to ease development of a Vue project during the course "Web programming" at the Faculty of Technical Sciences.
+
+Our professor forbids using Node.js, so in order to use SFCs and standard vue tooling I developed this tool.
+
+## This project relies on
+- [babel.js](https://babeljs.io/)
+- [vue/compiler-sfc](https://www.npmjs.com/package/@vue/compiler-sfc)
+- [terser](https://github.com/terser/terser)
+- [prettier](https://prettier.io/)
+
+## Examples
+
+### Transforming a .js file
+
+Input:
+
+`src/main.js`
+
+```js
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import './index.css'
+
+const app = createApp(App)
+
+app.use(router)
+app.mount('#app')
+```
+
+Output:
+
+`dist/src/main.js`
+```js
+import { createApp } from '/modules/vue@3.2.21.esm.min.prod.js'
+import App from './App.js'
+import router from './router/index.js'
+
+const app = createApp(App)
+
+app.use(router)
+app.mount('#app')
+```
+
+`dist/index.html`
+```html
+...
+<head>
+    <script type="module" src="src/main.js"></script>
+</head>
+
+<body>
+    <div id="app"></div>
+    <!-- built files will be auto injected -->
+</body>
+...
+```
+
+### Transforming a .vue file (SFC)
+
+Note: All style tags will be extracted into separate `.css` files and eagerly injected into
+the `index.html` entrypoint as stylesheets:
+
+`<link rel="stylesheet" href="/src/Component.css">`
+
+Input:
+
+`src/Component.vue`
+```js
+<template>
+  <div>
+    <button
+      @click="changeColor()"
+      :style="{color: msg}"
+    >
+      {{ msg }}
+    </button>
+    <input 
+      class="red"
+      v-model="msg"
+      :style="{color: msg}"
+    />
+    <AcademicCapIcon/>
+    <CogIcon/>
+    <CameraIcon/>
+  </div>
+</template>
+
+<script>
+import { ref, watch, onMounted } from 'vue'
+import { AcademicCapIcon, CogIcon } from '@heroicons/vue/solid'
+import { CameraIcon } from '@heroicons/vue/outline'
+
+export default { 
+  components: {
+    AcademicCapIcon,
+    CogIcon,
+    CameraIcon
+  },
+  setup() {
+    onMounted(() => console.log('Component Mounted'))
+
+    const counter = ref(0)
+    const msg = ref('red')
+    
+    const setMsg = value => msg.value = value
+    const changeColor = () => (counter.value++ % 2 == 0) ? setMsg('yellow') : setMsg('red')
+    watch(
+      () => msg,
+      () => console.log(`Color changed to ${msg.value}`)
+    )
+
+    return {
+      msg,
+      changeColor
+    }
+  }
+}
+</script>
+
+<style>
+.red {
+  color:orange;
+}
+</style>
+```
+
+Output:
+
+`dist/src/Component.js`
+
+```js
+import { onMounted, ref, watch } from '/modules/vue@3.2.21.esm.min.prod.js'
+import CameraIcon from '/modules/@heroicons/vue/outline/CameraIcon.js'
+import AcademicCapIcon from '/modules/@heroicons/vue/solid/AcademicCapIcon.js'
+import CogIcon from '/modules/@heroicons/vue/solid/CogIcon.js'
+
+export default {
+  template: `  
+    <div>
+      <button
+        @click="changeColor()"
+        :style="{color: msg}"
+      >
+        {{ msg }}
+      </button>
+      <input 
+        class="red"
+        v-model="msg"
+        :style="{color: msg}"
+      />
+      <AcademicCapIcon/>
+      <CogIcon/>
+      <CameraIcon/>
+    </div>
+  `,
+  components: { AcademicCapIcon, CogIcon, CameraIcon },
+  setup() {
+    onMounted(() => console.log('Component Mounted'))
+
+    const counter = ref(0)
+    const msg = ref('red')
+
+    const setMsg = value => (msg.value = value)
+    const changeColor = () => counter.value++ % 2 == 0 ? setMsg('yellow') : setMsg('red')
+    watch(
+      () => msg,
+      () => console.log(`Color changed to ${msg.value}`)
+    )
+
+    return {
+      msg,
+      changeColor
+    }
+  },
+}
+```
+
+`dist/Component.css`
+```css
+.red {
+  color:orange;
+}
+```
+
+## Limitations
+
+- Not very modular
+- File type loaders have to be changed in code
+
+### Supports:
+- Only vue@next
+- Only SFC components with 'export default {}'
+- Lazily loading components using import()
+
+### Does NOT support:
+- Lazily injecting component styles on load
+- `<script setup>`
+- `<style scoped>`
+- bindings in `<style>`
+
+
+## Configuration
+
+With no module resolution algorithms or bundling available, we have to specify
+module replacements manually.
+
+Example configuration:
+
+`imports.config.js`
+```js
+/*
+  Config for https://www.npmjs.com/package/babel-plugin-transform-imports
+*/
+const importOptions = {
+  'vue': {
+    transform: '/modules/vue@3.2.21.esm.min.prod.js',
+    skipDefaultConversion: true
+  },
+  '@vue/devtools-api': {
+    transform: '/modules/vue-devtools-api@6.0.0.esm.min.js',
+    skipDefaultConversion: true
+  },
+  'vue-router': {
+    transform: '/modules/vue-router@4.0.12.esm.min.js',
+    skipDefaultConversion: true
+  },
+  '@headlessui/vue': {
+    transform: '/modules/headlessui-vue@1.4.2.min.js',
+    skipDefaultConversion: true
+  },
+  '@heroicons/vue\/[^\/]*$': {
+    transform: (importName, matches) => `/modules/${matches[0]}/${importName}.js`,
+    skipDefaultConversion: false
+  },
+  'date-fns': {
+    transform: '/modules/date-fns@2.26.0.esm.min.js',
+    skipDefaultConversion: true
+  }
+}
+
+/*
+  Specify modules to be excluded from import transforms.
+  These should be 'leaves' of the dependency graph.
+  They should have no dependencies themselves.
+  This is a performance optimization.
+*/
+const excludedLibraries = [
+  'vue@3.2.21.esm.min.prod.js',
+  'tailwind@3.0.0-alpha.2.js',
+  'date-fns@2.26.0.esm.min.js'
+]
+
+export {
+  importOptions,
+  excludedLibraries
+}
+```
+## Usage
+
+Print usage:
+```console
+>> node index.js --help
+
+babel-transform-vue-ftn
+
+  Transpiles a vue-cli project into a non-bundled, non node-dependent project.
+
+Options
+
+  -i, --input string   The root directory of the project to transpile.
+  -h, --help           Print this usage guide.
+```
+
+Transpile a project:
+```console
+>> node index.js --input E:\Projects\web
+
+[ Building... ]
+[ Build finished ]: 759.051ms
+
+DONE Build complete. The dist directory E:/Projects/web/dist is ready to be deployed.
+```
---
+++ index.js
@@ -0,0 +1,421 @@
+import { transformSync } from '@babel/core'
+import babelPluginTransformImports from './babel-plugin-transform-imports/index.js'
+import { minify } from "terser"
+import dedent from 'dedent-js'
+import glob from 'glob'
+import fs from 'mz/fs.js'
+import prettier from 'prettier'
+import beautify from 'js-beautify'
+import { parse } from 'path'
+import chalk from 'chalk'
+import commandLineUsage from 'command-line-usage'
+import commandLineArgs from 'command-line-args'
+import { 
+  isObjectExpression,
+  objectProperty,
+  identifier,
+  templateLiteral,
+  templateElement,
+  isArrowFunctionExpression,
+  isCallExpression,
+  isImport
+} from '@babel/types'
+import { 
+  parse as parseSFC,
+  compileScript,
+  compileStyle
+} from 'vue/compiler-sfc'
+import { exit } from 'process'
+import url from 'url'
+
+const sections = [
+  {
+    header: 'babel-transform-vue-ftn',
+    content: 'Transpiles a vue-cli project into a non-bundled, non node-dependent project.'
+  },
+  {
+    header: 'Options',
+    optionList: [
+      {
+        name: 'input',
+        alias: 'i',
+        typeLabel: '{underline string}',
+        description: 'The root directory of the project to transpile.'
+      },
+      {
+        name: 'help',
+        alias: 'h',
+        description: 'Print this usage guide.',
+        type: Boolean
+      }
+    ]
+  }
+]
+const usage = commandLineUsage(sections)
+
+const optionDefinitions = [
+  { name: 'input', alias: 'i', type: String },
+  { name: 'help', alias: 'h', type: Boolean }
+]
+
+const options = commandLineArgs(optionDefinitions)
+
+if (Object.keys(options).length == 0 || options.help){
+  console.log(usage)
+  exit(1)
+} else if (!options.input) {
+  console.error('Must specify input directory.')
+  exit(1)
+}
+
+const cwd = options.input
+
+let importsConfigName = 'imports.config.js'
+
+try {
+  fs.statSync(cwd).isDirectory()
+} catch {
+  console.error(`Directory ${cwd} does not exist`)
+  exit(1)
+}
+
+if (!fs.existsSync(`${cwd}/${importsConfigName}`)) {
+  console.error(`No ${chalk.yellow(importsConfigName)} found at project root ${chalk.cyan(cwd)}`)
+  exit(1)
+}
+
+// Copy the config from the project root because node is retarded and cannot resolve remote
+// esm without thhe project package.json conatining type:module 
+
+let importsConfigPath = `${cwd}/${importsConfigName}`
+
+fs.copyFileSync(importsConfigPath, `./${importsConfigName}`)
+let { importOptions, excludedLibraries } = await import(`./${importsConfigName}`)
+fs.rmSync(`./${importsConfigName}`)
+
+const distPath = 'dist'
+const outDirectory = `${cwd}/${distPath}`
+
+const typesToLoad = ['vue', 'js', 'jpeg', 'jpg', 'png', 'svg', 'json', 'ico', 'woff2'].join('|')
+const aliases = [
+  {
+    alias: '@/',
+    replacement: '/src/'
+  }
+]
+
+console.time('[ Build finished ]')
+console.log()
+console.log('[ Building... ]')
+
+const indent = (content, amount) => {
+  return content
+    .split('\n')
+    .map(l => ' '.repeat(amount) + l)
+    .join('\n')
+}
+
+const hasAliases = importPath => {
+  for (const alias of aliases) {
+    if (importPath.startsWith(alias.alias)) return true 
+  }
+  return false
+}
+
+const replaceAliases = importPath => {
+  aliases.forEach(a => importPath = importPath.replace(a.alias, a.replacement))
+  return importPath
+}
+
+const renameImports = importPath => {
+  const importName = importPath.split('/').pop()
+  let replacement = importName.replace('.vue', '.js')
+  replaceAliases(replacement)
+  return importPath.replace(importName, replacement)
+}
+
+const createDistSubdir = (subdir = '') => {
+  if (subdir.startsWith('public')) return
+  fs.mkdirSync(`${outDirectory}/${subdir}`, {
+    recursive: true
+  })
+}
+
+const writeToDist = (subdir, data) => {
+  fs.writeFileSync(`${outDirectory}/${subdir}`, data)
+}
+
+const readFile = (filePath, fileName, fileType) => {
+  return fs.readFileSync(`${cwd}/${filePath}/${fileName}${fileType}`)
+}
+
+const babelPluginRemoveCssImports = {
+  visitor: {
+    ImportDeclaration: path => {
+      if (path.node.source.value.endsWith('.css')) {
+        path.remove()
+      }
+    }
+  }
+}
+
+const babelPluginTransformVueTemplate = template => ({
+  visitor: {
+    ExportDefaultDeclaration: path => {
+      const templateProperty = objectProperty(
+        identifier('template'),
+        templateLiteral(
+          [templateElement({ raw: template })],
+          []
+        )
+      )
+      if (isObjectExpression(path.node.declaration)) {
+        path.node.declaration.properties.unshift(templateProperty)
+      }
+    }
+  }
+})
+
+const babelPluginRenameImports = {
+  visitor: {
+    ImportDeclaration: path => {
+      path.node.source.value = replaceAliases(renameImports(path.node.source.value))
+    },
+    Function: (path, state) => {
+      if (
+          isArrowFunctionExpression(path.node) &&
+          isCallExpression(path.node.body)     &&
+          isImport(path.node.body.callee)
+      ) {
+        path.node.body.arguments[0].value = replaceAliases(renameImports(path.node.body.arguments[0].value))
+      }
+    }
+  }
+}
+
+const babelPluginTransformDirectoryImport = (workDir, subDir, fileTypes) => ({
+  visitor: {
+    ImportDeclaration: path => {
+
+      let source = path.node.source.value
+
+      const isAliased = hasAliases(source)
+
+      if (source[0] !== '.' && source[0] !== '/' && !isAliased) return
+
+      if (source.startsWith('./')) {
+        source = `${workDir}/${subDir}${source.slice(1)}`
+      } else {
+        source = `${workDir}/${replaceAliases(source)}`
+      }
+
+
+      if (fs.existsSync(source) && fs.lstatSync(source).isDirectory()) {
+        const files = fs.readdirSync(source)
+        for (const filePath of files) {
+          const { name, ext } = parse(filePath)
+          if (fileTypes.includes(ext.slice(1))) {
+            path.node.source.value = `${path.node.source.value}/${name}${ext}`
+          }
+        }
+      } else {
+        const fileName = source.split('/').pop()
+        const fileType = fileName.split('.').pop()
+        
+        if (fileType !== fileName) return // fileType is missing
+
+        source = source.replace(fileName, '')
+        const files = fs.readdirSync(source)
+
+        for (const filePath of files) {
+          const { name, ext } = parse(filePath)
+          if (name === fileName && fileTypes.includes(ext.slice(1))) {
+            const finalPath = path.node.source.value.replace(fileName, '')
+            path.node.source.value = renameImports(`${finalPath}${fileName}${ext}`)
+          }
+        }
+      }
+    }
+  }
+})
+
+let babelPlugins = [
+  babelPluginRenameImports,
+  babelPluginRemoveCssImports,
+  [babelPluginTransformImports, importOptions]
+]
+
+const prettierOptions = {
+  parser: 'babel',
+  semi: false,
+  vueIndentScriptAndStyle: true,
+  singleQuote: true,
+  arrowParens: 'avoid',
+  // printWidth: 200
+}
+
+const cssFiles = []
+
+const vueLoader = (filePath, fileName, fileType) => {
+
+  let script = readFile(filePath, fileName, fileType).toString()
+
+  const parsedSfc = parseSFC(script, { filename: fileName })
+
+  const hasScript = !!parsedSfc.descriptor.script
+
+  if (hasScript) {
+    script = compileScript(parsedSfc.descriptor, { id: fileName }).content
+  } else {
+    script = 'export default {}'
+  }
+  
+  const styles = parsedSfc.descriptor.styles[0]
+  const hasStyles = !!styles
+  
+  const compiledStyle = compileStyle({
+    id: fileName,
+    filename: fileName,
+    source: hasStyles && styles.content,
+  })
+  
+  let template = parsedSfc.descriptor.template.content.replace(/`/g, '\`') // escape all backticks
+  aliases.forEach(a => template = template.replaceAll(`src="${a.alias}`, `src="${a.replacement}`))
+  template = indent(template, 2)
+  const css = indent(compiledStyle.code, 2)
+  
+
+  const babelScript = transformSync(script, {
+    retainLines: true,
+    plugins: [
+      ...babelPlugins,
+      babelPluginTransformDirectoryImport(cwd, filePath, typesToLoad),
+      babelPluginTransformVueTemplate(template)
+    ]
+  })
+      
+  const prettierCode = prettier.format(babelScript.code, prettierOptions)
+  
+  writeToDist(`${filePath}/${fileName}.js`, prettierCode)
+  if (hasStyles) {
+    const finalName = `${filePath}/${fileName}.css`
+    cssFiles.push(finalName)
+    writeToDist(finalName, dedent(css))
+  }
+}
+
+const jsLoader = async (filePath, fileName, fileType) => {
+
+  const isSource = filePath.includes('src')
+
+  if (excludedLibraries.includes(fileName + fileType)) {
+    const fullPath = `${filePath}/${fileName}${fileType}` 
+    return fs.copyFileSync(`${cwd}/${fullPath}`, `${outDirectory}/${fullPath}`)
+  }
+
+  const script = readFile(filePath, fileName, fileType)
+  let babelScript = transformSync(script, {
+    ...(isSource && {
+      retainLines: true
+    }),
+    plugins: [
+      ...babelPlugins,
+      babelPluginTransformDirectoryImport(cwd, filePath, typesToLoad)
+    ]
+  })
+  babelScript = babelScript.code
+
+  if (isSource) {
+    babelScript = prettier.format(babelScript, prettierOptions)
+  } else {
+    babelScript = (await minify(babelScript)).code
+  }
+
+  writeToDist(`${filePath}/${fileName}${fileType}`, babelScript)
+}
+
+const assetLoader = (filePath, fileName, fileType) => {
+  const fullPath = `${filePath}/${fileName}${fileType}` 
+  fs.copyFileSync(`${cwd}/${fullPath}`, `${outDirectory}/${fullPath}`)
+}
+
+const iconLoader = (filePath, fileName, fileType) => {
+  fs.copyFileSync(`${cwd}/${filePath}/${fileName}${fileType}`, `${outDirectory}/${fileName}${fileType}`)
+}
+
+const loaders = [
+  {
+    test: '.vue',
+    use: vueLoader,
+  },
+  {
+    test: '.js',
+    use: jsLoader
+  },
+  {
+    test: /\.jpeg|\.jpg|\.png|\.svg|\.json|\.woff2/,
+    use: assetLoader
+  },
+  {
+    test: '.ico',
+    use: iconLoader
+  }
+]
+
+const buildEntrypoint = () => {
+  const stylesheets = cssFiles.map(url => `<link rel="stylesheet" href="${url}">`).join('\n')
+  const html = `
+    <!DOCTYPE html>
+    <html lang="">
+      <head>
+        <title></title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0">
+        <link rel="icon" href="favicon.ico">
+        <!-- Styles -->
+        ${stylesheets}
+        <!-- Entrypoint -->
+        <script type="module" src="src/main.js"></script>
+      </head>
+      <body>
+        <div id="app"></div>
+        <!-- built files will be auto injected -->
+      </body>
+    </html>
+  `
+  writeToDist('index.html', beautify.html(html))
+}
+
+const filenames = glob.sync(`**/*.?(${typesToLoad})`, {
+  cwd,
+  ignore: [
+    `*.json`,
+    `*.js`,
+    `.*`,
+    `node_modules/**/*`,
+    `dist/**/*`,
+    `${cwd}/node_modules/**/*`,
+    `${cwd}/dist/**/*`
+  ]
+})
+
+createDistSubdir()
+
+for (const filePath of filenames) {
+  const { dir: fileDirectory, name: fileName, ext: fileType } = parse(filePath)
+  createDistSubdir(fileDirectory)
+
+  for (const loader of loaders) {
+    if (fileType.match(loader.test)) {
+      loader.use(fileDirectory, fileName, fileType)
+    }
+  }
+}
+
+buildEntrypoint()
+
+console.timeEnd('[ Build finished ]')
+console.log()
+console.log(`${chalk.bgHex('#90fa28').black(' DONE ')} Build complete. The dist directory ${chalk.yellowBright(outDirectory.replaceAll('\\', '/'))} is ready to be deployed.`)
+console.log()
